92db7eb488585a92eee34d389f2b4ab3eddea0eb
Fangyi Streitel <fangyi.streitel@gmail.com>
2019-09-15 21:22:22 +0200

    Got all search results from one city with one rental yield threshold

--- src/main/java/OauthTesterIS24.java
+++ src/main/java/OauthTesterIS24.java
@@ -7,6 +7,7 @@ import java.net.URL;
 
 import com.google.gson.Gson;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import oauth.signpost.OAuthConsumer;
@@ -16,102 +17,117 @@ import oauth.signpost.basic.DefaultOAuthProvider;
 import oauth.signpost.exception.OAuthCommunicationException;
 import oauth.signpost.exception.OAuthExpectationFailedException;
 import oauth.signpost.exception.OAuthMessageSignerException;
+import oauth.signpost.exception.OAuthNotAuthorizedException;
 import sun.jvm.hotspot.debugger.posix.elf.ELFException;
 
-public class OauthTesterIS24 { public static void main(String[] args) throws Exception{
+public class OauthTesterIS24 {
 
-/*    OAuthConsumer consumer = new DefaultOAuthConsumer("Streitel-Immobilien-SucheKey", "GKYAbMEDamTVue0P");
-    OAuthProvider provider = new DefaultOAuthProvider("https://rest.immobilienscout24.de/restapi/security/oauth/request_token", "https://rest.immobilienscout24.de/restapi/security/oauth/access_token", "https://rest.immobilienscout24.de/restapi/security/oauth/confirm_access");
+    public static void main(String[] args) throws Exception{
 
-    System.out.println("Fetching request token...");
-    String authUrl = provider.retrieveRequestToken(consumer, "http://www.google.de");
-    String requestToken = consumer.getToken();
-    String requestTokenSecret = consumer.getTokenSecret();
-    System.out.println("Request token: " + requestToken);
-    System.out.println("Token secret: " + requestTokenSecret);
-    System.out.println("Now visit:\n" + authUrl + "\n... and grant this app authorization");
-    System.out.println("Enter the verification code and hit ENTER when you're done:");
-
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    String verificationCode = br.readLine();
-
-    System.out.println("Fetching access token...");
-    provider.retrieveAccessToken(consumer, verificationCode.trim());
-    String accessToken = consumer.getToken();
-
-    String accessTokenSecret = consumer.getTokenSecret();
-
-    System.out.println("Access token: " + accessToken);
-
-    System.out.println("Token secret: " + accessTokenSecret);
-
-    System.out.println("first call"); requestObjectApi(consumer);
-
-    System.out.println("second call"); requestObjectApi(consumer);
-
-    System.out.println("third call"); OAuthConsumer consumer2 = new DefaultOAuthConsumer("Streitel-Immobilien-SucheKey", "GKYAbMEDamTVue0P");
-
-    consumer2.setTokenWithSecret(accessToken, accessTokenSecret);
-
-    requestObjectApi(consumer2);*/
-
-    OAuthConsumer consumer3 = new DefaultOAuthConsumer("Streitel-Immobilien-SucheKey","GKYAbMEDamTVue0P");
-    consumer3.setTokenWithSecret("1119e9e5-d4e8-4838-be1e-37eb63034627", "tffH/7toC6rmL/Ho4f3SvdF6IJ/+kLKwE/4tJsiKMEbfSyYeTNAEiU/NMrS/i4jInn+uSXWFFsgfftQEWimPxaOHrG3HIgjphEdxSynUaIo=");
-    searchNewInterestingObject(consumer3);
+    //OauthGetTokenWithSecret();
+    OAuthConsumer consumer = new DefaultOAuthConsumer("Streitel-Immobilien-SucheKey","GKYAbMEDamTVue0P");
+    consumer.setTokenWithSecret("1119e9e5-d4e8-4838-be1e-37eb63034627", "tffH/7toC6rmL/Ho4f3SvdF6IJ/+kLKwE/4tJsiKMEbfSyYeTNAEiU/NMrS/i4jInn+uSXWFFsgfftQEWimPxaOHrG3HIgjphEdxSynUaIo=");
+    searchNewInterestingObject(consumer);
     }
-
 
     private static void searchNewInterestingObject(OAuthConsumer consumer) throws IOException, OAuthCommunicationException, OAuthMessageSignerException, OAuthExpectationFailedException{
 
         System.out.println("#################################################################################################");
 
-        URL urlBerlin = new URL("https://rest.immobilienscout24.de/restapi/api/search/v1.0/search/region?realestatetype=apartmentbuy&geocodes=1276003&price=0-100000&livingspace=50-1000");
+        double rentBerlin = 8.4;
 
-        HttpURLConnection apiRequest = (HttpURLConnection) urlBerlin.openConnection();
-        apiRequest.setRequestProperty("Accept","application/json");
 
-        consumer.sign(apiRequest);
-
-        apiRequest.connect();
-
-        InputStream inputStream = apiRequest.getInputStream();
-        String responseString = getStringFromStream(inputStream);
+        String urlBerlinString = "https://rest.immobilienscout24.de/restapi/api/search/v1.0/search/region?realestatetype=apartmentbuy&geocodes=1276003&price=0-150000&livingspace=50-1000";
+        String responseString = getResponseString(urlBerlinString,consumer);
 
         Gson gson = new Gson();
         JsonObject responseJson = gson.fromJson(responseString, JsonObject.class);
 
-        //System.out.println(responseJson);
-        int realEstateId = responseJson.getAsJsonObject("resultlist.resultlist").getAsJsonArray("resultlistEntries").get(0).getAsJsonObject().getAsJsonArray("resultlistEntry").get(0).getAsJsonObject().getAsJsonPrimitive("realEstateId").getAsInt();
-        //System.out.println(realEstateId);
-        String realEstateUrl = "https://www.immobilienscout24.de/expose/" + String.valueOf(realEstateId);
-        System.out.println(realEstateUrl);
+        int numberOfHits = responseJson.getAsJsonObject("resultlist.resultlist").getAsJsonObject("paging").getAsJsonPrimitive("numberOfHits").getAsInt();
+        System.out.println("number of Hits: " + numberOfHits);
+        int pageSize = responseJson.getAsJsonObject("resultlist.resultlist").getAsJsonObject("paging").getAsJsonPrimitive("pageSize").getAsInt();
+        System.out.println("pageSize: " + pageSize);
+        int numberOfPages = responseJson.getAsJsonObject("resultlist.resultlist").getAsJsonObject("paging").getAsJsonPrimitive("numberOfPages").getAsInt();
+        System.out.println("numberOfPages: " + numberOfPages);
+
+        // Search first page
+        getInterestingObjectFromOnePage(consumer, responseJson, pageSize, rentBerlin);
+
+        // Search pages from 2. to the last
+        for (int i = 2; i <= numberOfPages; i++){
+
+            System.out.println("page number: " + i);
+            String urlString = urlBerlinString + "&pagenumber=" + String.valueOf(i);
+
+            responseJson = gson.fromJson(getResponseString(urlString,consumer), JsonObject.class);
+            pageSize = responseJson.getAsJsonObject("resultlist.resultlist").getAsJsonObject("paging").getAsJsonPrimitive("pageSize").getAsInt();
+
+            getInterestingObjectFromOnePage(consumer, responseJson, pageSize, rentBerlin);
+        }
 
     }
 
-    private static void requestObjectApi(OAuthConsumer consumer) throws MalformedURLException, IOException, OAuthMessageSignerException, OAuthExpectationFailedException, OAuthCommunicationException, UnsupportedEncodingException {
+    private static void getInterestingObjectFromOnePage(OAuthConsumer consumer, JsonObject responseJson, int pageSize, double rent) throws OAuthExpectationFailedException, OAuthCommunicationException, OAuthMessageSignerException, IOException {
 
-        System.out.println("#################################################################################################");
+        JsonArray resultlistEntry = responseJson.getAsJsonObject("resultlist.resultlist").getAsJsonArray("resultlistEntries").get(0).getAsJsonObject().getAsJsonArray("resultlistEntry");
+        for (int i = 0; i < pageSize; i++) {
+            JsonObject realEstate = resultlistEntry.get(i).getAsJsonObject().getAsJsonObject("resultlist.realEstate");
+            int price = realEstate.getAsJsonObject("price").getAsJsonPrimitive("value").getAsInt();
+            int livingSpace = realEstate.getAsJsonPrimitive("livingSpace").getAsInt();
+            double rentalYield = 12 * livingSpace * rent / price;
+            if (rentalYield > 0.06) {
+                int realEstateId = resultlistEntry.get(i).getAsJsonObject().getAsJsonPrimitive("realEstateId").getAsInt();
+                String realEstateUrl = "https://www.immobilienscout24.de/expose/" + String.valueOf(realEstateId);
+                System.out.println(realEstateUrl);
+            }
+        }
 
-        URL url = new URL("https://rest.immobilienscout24.de/restapi/api/search/v1.0/search/region?realestatetype=apartmentbuy&geocodes=1276003");
+    }
 
-        HttpURLConnection apiRequest = (HttpURLConnection) url.openConnection();
-        apiRequest.setRequestProperty("Accept","application/json");
+    private static String getResponseString(String urlString, OAuthConsumer consumer) throws IOException, OAuthCommunicationException, OAuthExpectationFailedException, OAuthMessageSignerException {
+        URL urlBerlin = new URL(urlString);
+
+        HttpURLConnection apiRequest = (HttpURLConnection) urlBerlin.openConnection();
+        apiRequest.setRequestProperty("Accept", "application/json");
 
         consumer.sign(apiRequest);
 
         apiRequest.connect();
 
-        /*System.out.println("Expiration " + apiRequest.getExpiration());
-        System.out.println("Timeout " + apiRequest.getConnectTimeout());
-        System.out.println("URL " + url);
-        System.out.println("Method " + apiRequest.getRequestMethod());
-        System.out.println("Response: " + apiRequest.getResponseCode() + " " + apiRequest.getResponseMessage());*/
-
         InputStream inputStream = apiRequest.getInputStream();
-        String response = getStringFromStream(inputStream);
-        System.out.println(response);
+        return getStringFromStream(inputStream);
     }
 
+    private static void OauthGetTokenWithSecret() throws OAuthCommunicationException, OAuthExpectationFailedException, OAuthNotAuthorizedException, OAuthMessageSignerException, IOException {
+
+        OAuthConsumer consumer = new DefaultOAuthConsumer("Streitel-Immobilien-SucheKey", "GKYAbMEDamTVue0P");
+        OAuthProvider provider = new DefaultOAuthProvider("https://rest.immobilienscout24.de/restapi/security/oauth/request_token", "https://rest.immobilienscout24.de/restapi/security/oauth/access_token", "https://rest.immobilienscout24.de/restapi/security/oauth/confirm_access");
+
+        System.out.println("Fetching request token...");
+        String authUrl = provider.retrieveRequestToken(consumer, "http://www.google.de");
+        String requestToken = consumer.getToken();
+        String requestTokenSecret = consumer.getTokenSecret();
+        System.out.println("Request token: " + requestToken);
+        System.out.println("Token secret: " + requestTokenSecret);
+        System.out.println("Now visit:\n" + authUrl + "\n... and grant this app authorization");
+        System.out.println("Enter the verification code and hit ENTER when you're done:");
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String verificationCode = br.readLine();
+
+        System.out.println("Fetching access token...");
+        provider.retrieveAccessToken(consumer, verificationCode.trim());
+
+        String accessToken = consumer.getToken();
+        String accessTokenSecret = consumer.getTokenSecret();
+
+        System.out.println("Access token: " + accessToken);
+        System.out.println("Token secret: " + accessTokenSecret);
+
+        System.out.println("call: ");
+
+        System.out.println(getResponseString("https://rest.immobilienscout24.de/restapi/api/search/v1.0/search/region?realestatetype=apartmentbuy&geocodes=1276003",consumer));
+    }
 
     private static String getStringFromStream(InputStream inputStream) throws IOException {
         if (inputStream != null) {
